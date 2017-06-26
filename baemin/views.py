720a6b00bbeab11a2ad1caf0828cf4357811ce4b
sshong1991 <sshong1991@gmail.com>
2017-06-26 15:35:28 +0900

    가게목록 구현

--- baemin/views.py
+++ baemin/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from .models import Shop
 
 
 def index(request):
     return render(request, 'baemin/index.html', {
+        'shop_list': Shop.objects.all(),
     })
